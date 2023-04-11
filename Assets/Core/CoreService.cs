660341667a04a8a1b1ebde80b5efb8b9a2404166
andrey.mavrichev <andrey.mavrichev@playgendary.com>
2023-04-11 18:07:15 +0300

    test

---
+++ Assets/Core/CoreService.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CoreService
+{
+    public void CreateEntity()
+    {
+        var time = Time.time;
+        Debug.Log($"Time now {time} !!!!!");
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Main
+{
+    public void CreateService()
+    {
+        Debug.Log("CreateService");
+    }
+}
